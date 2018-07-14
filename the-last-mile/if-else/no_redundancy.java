4295f403f51f809a213fb8a4d4d4539ed03f6997
Shoupu Wan <shoupu_wan@apple.com>
2018-07-14 16:16:53 -0700

    Add if-else example

---
+++ the-last-mile/if-else/no_redundancy.java
@@ -0,0 +1,11 @@
+while (condition_overall) {
+    if (!condition1) {
+        statement3;
+        continue;
+    }
+    //assert condition1
+    if (condition2) {
+        statement1;
+    }
+    statement2;
+}
---
+++ the-last-mile/if-else/redundant_testing.java
@@ -0,0 +1,10 @@
+while (condition_overall) {
+    if (condition1 || condition2) {
+        if (condition1) {
+            statement1;
+        }
+        statement2;
+    } else {
+        statement3;
+    }
+}

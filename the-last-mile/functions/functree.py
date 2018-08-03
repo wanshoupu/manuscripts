819f6d2f556e8ae0d96f722cdb82a01673122dd7
Shoupu Wan <shoupu_wan@apple.com>
2018-08-03 12:30:30 -0700

    Explain and illustrate function tree

---
+++ the-last-mile/functions/functree.py
@@ -0,0 +1,40 @@
+def main():
+    if foo():
+        bar()
+    else:
+        baz()
+
+
+def bar():
+    ts = getNow()
+    if ts < 0:
+        print invokeRPC(ts)
+    else:
+        raise ValueError('ts < 0')
+
+
+def baz():
+    # TBD
+    pass
+
+
+def foo():
+    if cached():
+        print 'is cached'
+    else:
+        print 'is not true'
+
+
+def cached():
+    # TBD
+    pass
+
+
+def getNow():
+    # get the current timestamp
+    pass
+
+
+def invokeRPC(ts):
+    # Invokes an remote procedure call
+    pass

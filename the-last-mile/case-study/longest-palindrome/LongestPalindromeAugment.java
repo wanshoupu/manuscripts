55a4fe05cb419215840b74cf05ae58a3da57cad2
Shoupu Wan <shoupu_wan@apple.com>
2018-08-28 13:02:39 -0700

    Flesh out longest palindrome substring. Add palindrome symmetry discussion to aid following implementationgs

---
+++ the-last-mile/case-study/longest-palindrome/LongestPalindromeAugment.java
@@ -0,0 +1,42 @@
+/**
+ * O(N) solution with O(N) space
+ * The idea is to augment the original char array by inserting some
+ * 'blank' char as dummy palindromic center and restricting palindromic
+ * search to odd-length palindromes only
+ * @param str
+ * @return
+ */
+public static String longestPalindrome(String str){
+    final int n = str.length() * 2 + 1;
+    char[] aug = new char[n];
+    for(int i = 0; i < n; ++i){
+        aug[i] = (i & 1) == 0 ? 0 : str.charAt(i / 2);
+    }
+    int maxStart = 0, maxEnd = 0;
+    int[] length = new int[n];
+    length[0] = 1;
+    for(int center = 0, j = 1; j < n; ++j){
+        int wing = length[center] / 2;
+        if(j < center + wing){
+            int jmirror = 2 * center - j;
+            int jwing = length[jmirror] / 2;
+            if(jmirror - jwing > center - wing){
+                length[j] = length[jmirror];
+                continue;
+            }
+        }
+        //get the length of the longest palindrome centered on j
+        for(int i = center + wing + 1; ; i++){
+            if(2 * j - i < 0 || i == n || aug[2 * j - i] != aug[i]) {
+                length[j] = 2 * (i - 1 - j) + 1;
+                break;
+            }
+        }
+        center = j;
+        if(maxEnd - maxStart < length[center] / 2){
+            maxStart = (center + 1 - length[center] / 2) / 2;
+            maxEnd = (center + 1 + length[center] / 2) / 2;
+        }
+    } //end for
+    return str.substring(maxStart, maxEnd);
+}
---
+++ the-last-mile/case-study/longest-palindrome/LongestPalindromeInPlaceMax.java
@@ -0,0 +1,26 @@
+    public static String longestPalindrome(String str){
+        int maxCenter = 0;
+        //lengths stores the lengths of the longest palindrome around centers [0..n-1] in the original string
+        int[] lengths = new int[str.length() * 2 + 1];
+        for(int center = 0, j = 1; j < lengths.length; ++j){
+            final int jmirror = 2 * center - j;
+            if(j < center + lengths[center] && jmirror - lengths[jmirror] > center - lengths[center]){
+                    lengths[j] = lengths[jmirror];
+            }else{
+                //get the length of the longest palindrome centered on j
+                for(int i = (center + lengths[center]) / 2; ; i++){
+                    /* one would find that the above initialization may be changed to "(center + lengths[center] + 1) / 2" without any difference
+                     * This is because the value "(center + lengths[center])" is always even.
+                     */
+                    if(j - i - 1 < 0 || i == str.length() || str.charAt(j - i - 1) != str.charAt(i)) {
+                        lengths[j] = 2 * i - j;
+                        break;
+                    }
+                }
+                center = j;
+                maxCenter = lengths[maxCenter] < lengths[center] ? center : maxCenter;
+            }
+        } //end for
+        //substring starts at (center - lengths[center]) / 2, and ends exclusively at (center + lengths[center]) / 2 beautifully!!
+        return str.substring((maxCenter - lengths[maxCenter]) / 2, (maxCenter + lengths[maxCenter]) / 2);
+    }

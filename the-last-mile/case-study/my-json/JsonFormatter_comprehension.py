3fee4059086dc392b8a6604fc938befa571fd73f
Shoupu Wan <shoupu_wan@apple.com>
2018-09-07 14:10:33 -0700

    Buffer up a buggy version of Tex

--- the-last-mile/case-study/my-json/JsonFormatter_comprehension.py
+++ the-last-mile/case-study/my-json/JsonFormatter_comprehension.py
@@ -2,7 +2,7 @@ def __dictToPrettyString__(obj, indent):
     result = [__white__(indent + 1) +  # indentation prefix
               str(k) + ": " + toPrettyString(v, indent + 1) +  # field formatted
               # field separator (for all but last line) and newline.
-              (",\n" if n < len(obj) else "\n")
+              (",\n" if n < len(obj) - 1 else "\n")
               for n, (k, v) in enumerate(obj.items())]
     return "{\n" + "".join(result) + __white__(indent) + "}"
 
@@ -11,6 +11,6 @@ def __arrayToPrettyString__(obj, indent):
     result = [__white__(indent + 1)  # indentation prefix
               + toPrettyString(v, indent + 1) +  # field formatted
               # field separator (for all but last line) and newline.
-              (",\n" if n < len(obj) else "\n")
+              (",\n" if n < len(obj) - 1 else "\n")
               for n, v in enumerate(obj)]
     return "[\n" + "".join(result) + __white__(indent) + "]"
--- the-last-mile/case-study/my-json/NewJsonFormatter.py
+++ the-last-mile/case-study/my-json/NewJsonFormatter.py
@@ -6,13 +6,13 @@ formatter = Formatter()
 def __dictToPrettyString__(obj):
     items = [formatter.newline + str(k) + ": " + toPrettyString(v)
              for k, v in obj.items()]
-    return "{" + join(items) + "}"
+    return "{" + formatItems(items) + "}"
 
 
 def __arrayToPrettyString__(obj):
     items = [formatter.newline + toPrettyString(v) for v in obj]
-    return "[" + join(items) + "]"
+    return "[" + formatItems(items) + "]"
 
 
-def join(items):
-    return formatter.indent(",".join(items)) + formatter.newline
+def formatItems(items):
+    return formatter.indent(','.join(items)) + formatter.newline

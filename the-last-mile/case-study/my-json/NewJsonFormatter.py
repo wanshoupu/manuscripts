088d10ab5c520cdff8e2d350edb2be403ce95d91
Shoupu Wan <shoupu_wan@apple.com>
2018-09-25 16:15:43 -0700

    Complete the section for JSON formatting with a new model of indentation

--- the-last-mile/case-study/my-json/NewJsonFormatter.py
+++ the-last-mile/case-study/my-json/NewJsonFormatter.py
@@ -3,12 +3,27 @@ from Formatter import Formatter
 formatter = Formatter()
 
 
-def __dictToPrettyString__(obj):
+def toPrettyString(obj):
+    """
+    Input may be of type: string, number, boolean, dictionary, or None.
+    When it's a dictionary, its keys are of type string and values can be
+    :param obj obj represents an in-memory data structure of a JSON
+    :param indent the overall indent value, default to 0
+    :return: string format of my_json
+    """
+    if type(obj) is list:
+        return arrayToPrettyString(obj)
+    if issubclass(type(obj), dict):
+        return dictToPrettyString(obj)
+    return str(obj)
+
+
+def dictToPrettyString(obj):
     items = [formatter.newline + str(k) + ": " + toPrettyString(v)
              for k, v in obj.items()]
     return "{" + formatter.formatElements(items) + "}"
 
 
-def __arrayToPrettyString__(obj):
+def arrayToPrettyString(obj):
     items = [formatter.newline + toPrettyString(v) for v in obj]
     return "[" + formatter.formatElements(items) + "]"

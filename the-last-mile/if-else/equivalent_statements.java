a275f3f60082e79b7a386d2531b499b1703fff66
Shoupu Wan <shoupu_wan@apple.com>
2018-10-21 22:46:56 -0700

    Chapter If-else: Formatting side-by-side two-column comparison part with minipage with top-alignment; clean up the chapter and code snippet

--- the-last-mile/if-else/equivalent_statements.java
+++ the-last-mile/if-else/equivalent_statements.java
@@ -8,6 +8,6 @@ sb.append(str.substring(i, j + 1));
 sb.append(str, i, j + 1);
 
 
-Arrays.asList(new String\[\]\{"a", "an", "em", "fir", "fire", "ire", "ma", "man"\})
+Arrays.asList(new String[]\{"a", "an", "the"\})
 
-Arrays.asList("a", "an", "em", "fir", "fire", "ire", "ma", "man")
+Arrays.asList("a", "an", "the")

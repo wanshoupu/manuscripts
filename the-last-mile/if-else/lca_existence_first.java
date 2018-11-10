95846fc4dafa5d659ffa0f36ad473d130603f8b0
Shoupu Wan <shoupu_wan@apple.com>
2018-11-09 16:50:03 -0800

    Finish up the presentation of case studies for constructing cascade conditionals

--- the-last-mile/if-else/lca_existence_first.java
+++ the-last-mile/if-else/lca_existence_first.java
@@ -1,10 +1,10 @@
 Node findAncestor(Node root, int a, int b) {
     if (root == null) return null;
+    if (find(root, a) == null || find(root, b) == null)
+        return null;
     if (a < root.data && b < root.data)
         return findAncestor(root.left, a, b);
     if (a > root.data && b > root.data)
         return findAncestor(root.right, a, b);
-    if (find(root, a) == null || find(root, b) == null)
-        return null;
     return root;
 }
--- the-last-mile/if-else/lca_straddle_first.java
+++ the-last-mile/if-else/lca_straddle_first.java
@@ -1,5 +1,7 @@
 Node findAncestor(Node root, int a, int b) {
     if (root == null) return null;
+    if (Math.max(a,b) >= root.data || Math.min(a,b) <= root.data)
+        return root
     if (a < root.data && b < root.data)
         return findAncestor(root.left, a, b);
     if (a > root.data && b > root.data)

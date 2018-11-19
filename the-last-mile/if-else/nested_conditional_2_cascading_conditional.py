bdd186d4dc35bc7b2894e134738ff3af3664cdd3
Shoupu Wan <shoupu_wan@apple.com>
2018-11-19 15:55:55 -0800

    Complete section unnest nested conditional

--- the-last-mile/if-else/nested_conditional_2_cascading_conditional.py
+++ the-last-mile/if-else/nested_conditional_2_cascading_conditional.py
@@ -1,25 +1,37 @@
 # nested conditional
-
-if n not in lookup:
-    heappush(lookup[n + 1], 1)
+key = 3
+if key not in lookup:
+    heappush(lookup[key + 1], 1)
 else:
-    vals = lookup[n]
+    vals = lookup[key]
     # pop the top element, increment it,
     newVal = heappop(vals) + 1
     if not vals:
-        lookup.pop(n)
+        lookup.pop(key)
     # push newVal back
-    heappush(lookup[n + 1], newVal)
+    heappush(lookup[key + 1], newVal)
 
 # Refactored cascading conditional
-if n not in lookup:
-    heappush(lookup[n + 1], 1)
-elif len(lookup[n]) == 1:
-    val, = lookup.pop(n)  # unpack into val
-    heappush(lookup[n + 1], val + 1)
+if key not in lookup:
+    heappush(lookup[key + 1], 1)
+elif len(lookup[key]) == 1:
+    val, = lookup.pop(key)  # unpack into val
+    heappush(lookup[key + 1], val + 1)
 else:
     # pop the top element, increment it,
-    newVal = heappop(lookup[n]) + 1
+    newVal = heappop(lookup[key]) + 1
     # push newVal back
-    heappush(lookup[n + 1], newVal)
+    heappush(lookup[key + 1], newVal)
 
+
+# reduce duplicate code
+if key not in lookup:
+    seq = 1
+elif len(lookup[key]) == 1:
+    # pop the entry, get first element and increment it,
+    seq = lookup.pop(key)[0] + 1
+else:
+    # pop the top element, increment it,
+    seq = hp.heappop(lookup[key]) + 1
+# push seq back
+hp.heappush(lookup[key + 1], seq)

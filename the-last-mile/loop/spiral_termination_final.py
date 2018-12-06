11dd4aa7056505a469d726ca4a5017f7825a109e
Shoupu Wan <shoupu_wan@apple.com>
2018-12-05 22:02:57 -0800

    Extend the presentation of loop initialization

--- the-last-mile/loop/spiral_termination_final.py
+++ the-last-mile/loop/spiral_termination_final.py
@@ -1,16 +1,12 @@
 def spiral_term(m, n):
-    x, y = 0, -1
-    while m and n:
-        if 1 == m:
-            y += n
-            m = 0
-        elif 1 == n:
-            x += m - 1
-            y += 1
-            n = 0
-        else:
-            x += 1
-            y += 1
-            m -= 2
-            n -= 2
-    return x, y
+    i, j = 0, 0
+    while 2 < m and 2 < n:
+        i += 1
+        j += 1
+        m -= 2
+        n -= 2
+    if m == 1:
+        return i, j + n - 1
+    if n == 1:
+        return i + m - 1, j
+    return i + 1, j

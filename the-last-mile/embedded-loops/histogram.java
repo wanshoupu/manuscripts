74abeaacde30e5a03811d62cf9a130f2454937b1
Shoupu Wan <shoupu_wan@apple.com>
2018-12-10 10:31:50 -0800

    Move histogram code to embedded-loops folder

---
+++ the-last-mile/embedded-loops/histogram.java
@@ -0,0 +1,15 @@
+public int largestRectangleArea(int[] heights) {
+    Stack<Integer> stack = new Stack<>();
+    int maxRec = 0;
+    for (int i = 0; i <= heights.length; ) {
+        if (!stack.isEmpty() && (i == heights.length || heights[stack.peek()] >= heights[i])) {
+            int height = heights[stack.pop()];
+            int width = stack.isEmpty() ? i : i - stack.peek() - 1;
+            maxRec = Math.max(maxRec, height * width);
+        } else {
+            stack.push(i);
+            ++i;
+        }
+    }
+    return maxRec;
+}

234513b319a346d4eb4123632a311a9484e65904
Shoupu Wan <shoupu_wan@apple.com>
2019-01-02 13:07:10 -0800

    Rewrite of sentinels section quick sort

--- the-last-mile/sentinels/qsort_part_sentinel.c
+++ the-last-mile/sentinels/qsort_part_sentinel.c
@@ -2,13 +2,13 @@ int* part(int* s, int*e) {
     init_swap(s, e);
     int* const pivot = s;
     while(true) {
-         while(*++s < *pivot);   // find the next element >= pivot
-         while(*--e > *pivot); // find the prev element <= pivot
-         if (s < e) {
-             swap(s, e); //swap the out-of-place elements
-         } else {
-             swap(pivot, e);
-             return e;
-         }
+        while(*++s < *pivot);   // find the next element >= pivot
+        while(*--e > *pivot); // find the prev element <= pivot
+        if (s < e) {
+            swap(s, e); //swap the out-of-place elements
+        } else {
+            swap(pivot, e);
+            return e;
+        }
     }
 }

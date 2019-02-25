ae74f825a2a90b160b37beeaee0950d44d0bbb63
Shoupu Wan <shoupu_wan@apple.com>
2019-02-24 19:43:43 -0800

    Pseudo-code init_swap function for quicksort

--- the-last-mile/sentinels/qsort_init_swap.c
+++ the-last-mile/sentinels/qsort_init_swap.c
@@ -1,16 +1,6 @@
 void init_swap(int* s, int* e) {
-    // could be as simple as swap(s, s + rand() % (e - s));
-    for (int i = 0; i < 3; i++) {
-        swap(s + i, s + i + rand() % (e - s - i));
-    } // elect 3 elements and put them at positions: s, s+1, s+2
-    if (*s < *(s + 1)) {
-        swap(s, s + 1);
-    }
-    if (*(s + 2) < *(s + 1)) {
-        swap(s + 1, s + 2);
-    } // s + 1 has the minimum of the 3
-    if (*(s + 2) < *s) {
-        swap(s, s + 2);
-    } // s, the pivot, has the median of the 3
-    swap(e - 1, s + 2); // e - 1, has the maximum of the 3
+    // elect 3 elements
+    // sort them as [min, median, max]
+    // deploy them at positions: s, (s + 1), and (e - 1)
+    ...
 }

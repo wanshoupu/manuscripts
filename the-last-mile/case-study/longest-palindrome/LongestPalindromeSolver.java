3c380c653ca566d45198539f18bb9f36511174c6
Shoupu Wan <shoupu_wan@apple.com>
2020-03-03 09:47:09 -0800

    Add an arithmetic semantic table

--- the-last-mile/case-study/longest-palindrome/LongestPalindromeSolver.java
+++ the-last-mile/case-study/longest-palindrome/LongestPalindromeSolver.java
@@ -3,75 +3,68 @@ package org.shoupu.string;
 public class LongestPalindromeSolver {
 
     private String input;
-    private int[] lengths;
+    // Specifications of palindromic substrings in the input:
+    // The i-th element in array 'lsp' records the max length of palindrome centered
+    // on a char or between two adjacent chars of the input string depends on the parity of i:
+    // if i is even, it is centered between the (i/2)th char and the (i/2 + 1)th char;
+    // if i is odd, it is centered on the (i-1)/2th char in string input
+    // lps[0] = 0 by definition
+    private int[] lps;
 
-    /**
-     * Algorithm:
-     *
-     * @param s
-     * @return
+    /*
+     * Find the (first) longest palindromic substring in input string
      */
-    public String longestPalindrome(String s) {
-        this.input = s;
-        // Each of the palindrome substrings in s is completely specified by the index and the
-        // element in lengths as follows (zero-based index should be understood):
-        //
-        // The i-th element in array `lengths' records the max length of palindrome centered
-        // on a char or centered between two neighboring chars in string input depends on the parity of i:
-        // if i is even, it is centered between the (i/2)th char and the (i/2 + 1)th char in string input;
-        // if i is odd, it is centered on the (i-1)/2th char in string input
-        // lengths[0] = 0 by definition
-        lengths = new int[s.length() * 2 + 1];
+    public String longestPalindrome(String input) {
+        this.input = input;
+        lps = new int[input.length() * 2 + 1];
         solve();
-        return constructResult();
+        return substring(argmax());
     }
 
-    /**
-     * Solve the longest palindrome problem and store the result in field lengths
+    /*
+     * Solve the longest palindrome problem and cache the result
      */
-    private void solve() {
-        for (int i = 1, irightmost = 0; i < lengths.length; ++i) {
-            // irightmost is the index of palindrome that extends to the rightmost
-            if (getRightBound(irightmost) <= i) {
-                lengths[i] = palength(i, i);
-                irightmost = i; // i becomes the rightmost palindrome
+    void solve() {
+        for (int i = 1, rm = 0; i < lps.length; ++i) {
+            // rm is the index of palindrome that extends to the rightmost
+            if (getRightBound(rm) <= i) {
+                lps[i] = palength(i, i);
+                rm = i; // i becomes the rightmost palindrome
                 continue;
             }
-            int mi = toMirrorImage(irightmost, i);
+            int mi = toMirrorImage(rm, i);
             //assert mi >= 0
-            if (getLeftBound(mi) > getLeftBound(irightmost)) {
-                // if the reflection palindrome is covered entirely by that at irightmost
+            if (getLeftBound(mi) > getLeftBound(rm)) {
+                // if the reflection palindrome is covered entirely by that at rm
                 // then no calculation is needed
-                lengths[i] = lengths[mi];
+                lps[i] = lps[mi];
             } else {
-                //calculate the part outside of the right bound of the palindrome at irightmost
-                lengths[i] = palength(i, getRightBound(irightmost));
-                irightmost = i;
+                //calculate the part outside of the right bound of the palindrome at rm
+                lps[i] = palength(i, getRightBound(rm));
+                rm = i;
             }
         }
     }
 
-    /**
-     * Find the maximum palindromic substring based on array {@link #lengths}
-     *
-     * @return
+    /*
+     * Find the index of maximum palindromic substring
      */
-    private String constructResult() {
+    int argmax() {
         int maxi = 0;
-        for (int i = 0; i < lengths.length; ++i) {
-            if (lengths[i] > lengths[maxi]) {
+        for (int i = 0; i < lps.length; ++i) {
+            if (lps[i] > lps[maxi]) {
                 maxi = i;
             }
         }
-        return substring(maxi);
+        return maxi;
     }
 
-    private int getLeftBound(int i) {
-        return i - lengths[i];
+    int getLeftBound(int i) {
+        return i - lps[i];
     }
 
-    private int getRightBound(int i) {
-        return i + lengths[i];
+    int getRightBound(int i) {
+        return i + lps[i];
     }
 
     String substring(int center) {
@@ -82,41 +75,33 @@ public class LongestPalindromeSolver {
         return input.substring(left, right);
     }
 
-    /**
+    /*
      * Find the palindrome in string input centered at center.
      * Note that the portion between center and index has already been proven palindromic
      * so this function will skip checking that part
-     *
-     * @param center
-     * @param index
-     * @return
      */
-    private int palength(int center, int index) {
+    int palength(int center, int index) {
         for (int mi = toMirrorImage(center, index); ; ++index, --mi) {
             // if one of the conditions:
-            // reached the left end of input string
-            // reached the right end of input string
+            // reaching the left end of input string
+            // reaching the right end of input string
             // encountered char mismatch
-            if (mi < 0 || index >= lengths.length || isMismatch(index, mi)) {
+            if (mi < 0 || index >= lps.length || isMismatch(index, mi)) {
                 return index - center - 1;
             }
         }
     }
 
-    /**
+    /*
      * Determine if there is a mismatch between virtual char at i and j
      * If i is even (so is j), there is no mismatch as virtual chars are always a match
-     *
-     * @param i
-     * @param j
-     * @return
+     * @return true if mismatch false otherwise
      */
-    private boolean isMismatch(int i, int j) {
+    boolean isMismatch(int i, int j) {
         return (i & 1) == 1 && input.charAt(i / 2) != input.charAt(j / 2);
     }
 
-    private static int toMirrorImage(int axis, int x) {
+    int toMirrorImage(int axis, int x) {
         return 2 * axis - x;
     }
-
 }

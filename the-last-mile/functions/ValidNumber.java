de052865ff6a33791fbb42edf5c054a136bd1d3d
Shoupu Wan <shoupu_wan@apple.com>
2020-03-21 13:07:58 -0700

    Add some sentences to functions.tex and update the code to illustrate the stark contrast between monolithic code and modular solution

--- the-last-mile/functions/ValidNumber.java
+++ the-last-mile/functions/ValidNumber.java
@@ -1,111 +1,43 @@
 package org.shoupu.string;
 
+import java.util.Arrays;
+
 public class ValidNumber {
-    public class Solution {
-        public boolean isNumber(String s) {
-            s = s.trim();
-            s = s.toLowerCase();
-            if (s.contains("e")) {
-                return isScientific(s);
-            }
-            if (s.contains(".")) {
-                return isFloat(s);
-            }
-            return isInteger(s);
+    public boolean isNumber(String string) {
+        String string1 = string.trim().toLowerCase();
+        if (string1.contains("e")) {
+            return isScientific(string1);
         }
-
-        boolean isScientific(String s) {
-            String[] parts = s.split("e", 2);
-            if (parts[0].contains(".")) {
-                return isFloat(parts[0]) && isInteger(parts[1]);
-            } else {
-                return isInteger(parts[0]) && isInteger(parts[1]);
-            }
+        if (string1.contains(".")) {
+            return isFloat(string1);
         }
-
-        boolean isFloat(String s) {
-            if (s.startsWith("+") || s.startsWith("-")) {
-                s = s.substring(1);
-            }
-            String[] parts = s.split("\\.", 2);
-            if (parts[0].isEmpty() && parts[1].isEmpty()) return false;
-            return (parts[0].isEmpty() || isUnsigned(parts[0]))
-                    && (parts[1].isEmpty() || isUnsigned(parts[1]));
-        }
-
-        boolean isInteger(String s) {
-            if (s.startsWith("+") || s.startsWith("-")) {
-                s = s.substring(1);
-            }
-            return isUnsigned(s);
-        }
-
-        boolean isUnsigned(String s) {
-            if (s.isEmpty()) return false;
-            for (char c : s.toCharArray()) {
-                if (c < '0' || c > '9') return false;
-            }
-            return true;
-        }
-    }
-
-    public boolean isNumber(String s) {
-        s = s.trim().toLowerCase();
-        if (s.isEmpty()) return false;
-        if (s.charAt(0) == '-' || s.charAt(0) == '+') {
-            s = s.substring(1);
-        }
-        return isScientific(s);
+        return isInteger(string1);
     }
 
     boolean isScientific(String s) {
-        if (s.isEmpty()) {
-            return false;
-        }
-        if (s.contains("e")) {
-            String[] parts = s.split("e", 2);
-            return isDotFloat(parts[0]) && isInteger(parts[1]);
-        }
-        return isDotFloat(s);
+        String[] parts = s.split("e", 2);
+        return isFloat(parts[0]) && isInteger(parts[1]);
     }
 
-    boolean isDotFloat(String s) {
-        if (s.isEmpty()) {
-            return false;
+    boolean isFloat(String s) {
+        if (s.startsWith("+") || s.startsWith("-")) {
+            s = s.substring(1);
         }
-        if (s.startsWith(".")) {
-            return isUnsignedInteger(s.substring(1));
+        String[] parts = s.split("\\.", 2);
+        if (parts.length == 1) {
+            return parts[0].matches("\\d+");
         }
-        if (s.endsWith(".")) {
-            return isUnsignedInteger(s.substring(0, s.length() - 1));
-        }
-        if (s.contains(".")) {
-            String[] parts = s.split("\\.", 2);
-            return isUnsignedInteger(parts[0]) && isUnsignedInteger(parts[1]);
-        }
-        return isUnsignedInteger(s);
+        return Arrays.stream(parts).allMatch(p -> p.isEmpty() || p.matches("\\d+"));
     }
 
     boolean isInteger(String s) {
-        if (s.isEmpty()) {
-            return false;
-        }
-        if (s.charAt(0) == '-' || s.charAt(0) == '+') {
+        if (s.startsWith("+") || s.startsWith("-")) {
             s = s.substring(1);
         }
-        return isUnsignedInteger(s);
-
+        return isUnsigned(s);
     }
 
-    boolean isUnsignedInteger(String s) {
-        if (s.isEmpty()) {
-            return false;
-        }
-        for (char c : s.toCharArray()) {
-            if (c < '0' || c > '9') {
-                return false;
-            }
-        }
-        return true;
+    boolean isUnsigned(String s) {
+        return s.matches("\\d+");
     }
 }
--- the-last-mile/functions/ValidNumberToBeRefactored.java
+++ the-last-mile/functions/ValidNumberToBeRefactored.java
@@ -1,17 +1,27 @@
 package org.shoupu.string;
 
+import java.util.Arrays;
+
 public class ValidNumber {
-    public boolean isNumber(String s) {
-        s = s.trim();
-        s = s.toLowerCase();
-        if (s.contains("e")) {
-            String[] parts = s.split("e", 2);
+    public boolean isNumber(String string) {
+        String string1 = string.trim().toLowerCase();
+        if (string1.contains("e")) {
+            String[] parts = string1.split("e", 2);
             if (parts[0].contains(".")) {
                 String s1 = parts[1];
                 if (s1.startsWith("+") || s1.startsWith("-")) {
                     s1 = s1.substring(1);
                 }
-                return isFloat(parts[0]) && s1.matches("\\d+");
+                if (!s1.matches("\\d+")) return false;
+                String s0 = parts[0];
+                if (s0.startsWith("+") || s0.startsWith("-")) {
+                    s0 = s0.substring(1);
+                }
+                String[] parts1 = s0.split("\\.", 2);
+                if (parts1.length == 1) {
+                    return parts1[0].matches("\\d+");
+                }
+                return Arrays.stream(parts1).allMatch(p -> p.isEmpty() || p.matches("\\d+"));
             } else {
                 String s1 = parts[1];
                 if (s1.startsWith("+") || s1.startsWith("-")) {
@@ -24,24 +34,20 @@ public class ValidNumber {
                 return s2.matches("\\d+") && s1.matches("\\d+");
             }
         }
-        if (s.contains(".")) {
-            return isFloat(s);
+        if (string1.contains(".")) {
+            if (string1.startsWith("+") || string1.startsWith("-")) {
+                string1 = string1.substring(1);
+            }
+            String[] parts = string1.split("\\.", 2);
+            if (parts.length == 1) {
+                return parts[0].matches("\\d+");
+            }
+            return Arrays.stream(parts).allMatch(p -> p.isEmpty() || p.matches("\\d+"));
         }
-        String s1 = s;
+        String s1 = string1;
         if (s1.startsWith("+") || s1.startsWith("-")) {
             s1 = s1.substring(1);
         }
         return s1.matches("\\d+");
     }
-
-    boolean isFloat(String s) {
-        if (s.startsWith("+") || s.startsWith("-")) {
-            s = s.substring(1);
-        }
-        String[] parts = s.split("\\.", 2);
-        if (parts[0].isEmpty() && parts[1].isEmpty()) return false;
-        return (parts[0].isEmpty() || parts[0].matches("\\d+"))
-                && (parts[1].isEmpty() || parts[1].matches("\\d+"));
-    }
-
 }
